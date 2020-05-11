415afbaf797f4ad66dd2c2086a547c7ff571a5e8
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-05-11 11:40:01 -0400

    created method to get products from db

--- src/store/models.py
+++ src/store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
-    slug = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category', blank=True)
 
@@ -16,7 +16,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
-    slug = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- src/store/urls.py
+++ src/store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('<slug:category_slug>', views.home, name="product_by_category"),
     path("product/", views.product, name="product")
 ]
